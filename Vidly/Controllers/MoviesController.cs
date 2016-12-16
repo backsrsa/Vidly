49bf92661285617ac19805804447c53be8011102
andres <andres.rsa@outlook.com>
2016-12-16 00:37:34 -0600

    Creacion de un modelo de movie una custom route y configuracion para parametros especifos

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Batman!!!" };
+            return View(movie);
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            return Content("Id= " + id);
+        }
+
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        //movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            if (string.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+            return Content($"pageIndex={pageIndex}&sortBy={sortBy}");
+        }
+    }
+}
